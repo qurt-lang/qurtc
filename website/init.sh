babefff6647326e69cbe914a9c7b6c1e72ca8cec
Nurtay Tolymbek <nurtay.tolymbek@netcracker.com>
2025-09-29 00:30:13 +0500

    fix(website): change service to website

--- website/init.sh
+++ website/init.sh
@@ -38,13 +38,6 @@ data_path="./nginx/certbot"
 email=""
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
-    read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-    if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-        exit
-    fi
-fi
-
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
     echo "### Downloading recommended TLS parameters ..."
     mkdir -p "$data_path/conf"
@@ -64,7 +57,7 @@ docker compose -f "docker-compose.yml" run --rm --entrypoint "\
 echo
 
 echo "### Starting nginx ..."
-docker compose  -f "docker-compose.yml" up --force-recreate -d nginx
+docker compose  -f "docker-compose.yml" up --force-recreate -d website
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
